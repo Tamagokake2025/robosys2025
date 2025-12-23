a996ad972cea7987a8918528a3e292229ad0f649
Kei Take <S24C1076MR@s.chibakoudai.jp>
2025-12-24 04:30:36 +0900

    test.bash編集

--- test.bash
+++ test.bash
@@ -14,33 +14,47 @@ prog="./alnum"
 
 ### Normal Input ###
 out=$(echo "c d y " | $prog)
+[ "$?" = 0 ] || ng "$LINENO"
 [ "${out}" = "3 4 25" ] || ng "$LINENO"
 
 out=$(echo "apple kei" | $prog)
+[ "$?" = 0 ] || ng "$LINENO"
 [ "${out}" = "1 16 16 12 5 11 5 9" ] || ng "$LINENO"
 
 out=$(echo "7 19 26" | $prog)
+[ "$?" = 0 ] || ng "$LINENO"
 [ "${out}" = "g s z" ] || ng "$LINENO"
 
 out=$(echo "ro 8 20 qju" | $prog)
+[ "$?" = 0 ] || ng "$LINENO"
 [ "${out}" = "18 15 h t 17 10 21" ] || ng "$LINENO"
 
 out=$(echo @ / . = | $prog)
+[ "$?" = 0 ] || ng "$LINENO"
 [ "${out}" = "? ? ? ?" ] || ng "$LINENO"
 
 out=$(echo "27 32 43 57" | $prog)
+[ "$?" = 0 ] || ng "$LINENO"
 [ "${out}" = "? ? ? ?" ] || ng "$LINENO"
 
 out=$(echo "こんにちは" | $prog)
+[ "$?" = 0 ] || ng "$LINENO"
 [ "${out}" = "12371 12435 12395 12385 12399" ] || ng "$LINENO"
 
 out=$(echo "! 12371 12435 12395 12385 12399" | $prog)
+[ "$?" = 0 ] || ng "$LINENO"
 [ "${out}" = "!こんにちは" ] || ng "$LINENO"
 
 out=$(echo "! 73 1 13 fine" | $prog)
+[ "$?" = 0 ] || ng "$LINENO"
 [ "${out}" = "!Iamfine" ] || ng "$LINENO"
 
-out=$(echo | $prog)         
+out=$(echo | $prog)
+[ "$?" = 0 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo ( ) | $prog)
+[ "$?" != 0 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 ### 結果 ###
